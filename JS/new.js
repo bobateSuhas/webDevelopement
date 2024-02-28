7cd0a9f58e7e2563f9257dc527bb9388830c0a40
bobateSuhas <suhasbobate@gmail.com>
2024-02-28 14:34:44 +0530

    my first project

---
+++ JS/new.js
@@ -0,0 +1,10 @@
+ 
+ const profile={
+          fullName: "shradhaKhapara",
+          isFollow: true,
+          posts:195,followers:56900,following:4,
+          message:0,
+ }
+ console.log(profile)
+ console.log(typeof profile.fullName);
+ console.log(typeof profile.posts)
